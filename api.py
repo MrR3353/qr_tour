9f87011c6b51a8616d17d8450aa3a6963efdc663
Vadim <vad.general@mail.ru>
2023-11-24 22:04:55 +0300

    Fix: добавить обработку ошибок для API

--- api.py
+++ api.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 import db
 
 app = Flask(__name__)
 
-
 @app.route('/<int:qr_id>', methods=['GET'])
 def index(qr_id):
-    return db.get_by_id(qr_id)
-
+    try:
+        return db.get_by_id(qr_id)
+    except Exception as e:
+        error_message = f"An error occurred: {str(e)}"
+        return jsonify({'status': '1', 'message': error_message}), 400
 
 if __name__ == '__main__':
     app.run(port=5000)
-
-
